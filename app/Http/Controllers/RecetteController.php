c834965e8c3ad4bdb5a51ea26486301017f6093e
samuel_tittelein <samuel_tittelein@ens.univ-artois.fr>
2024-11-20 10:54:22 +0100

    methode du controleur pour manipuler des recettes

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -19,4 +19,58 @@ class RecetteController extends Controller
     public function recette($recette) {
         return view('recettes.recette', ['recette' => $recette]);
     }
+
+    public function create() {
+        return view('recettes.create', ['titre' => "Création d'une recette"]);
+    }
+
+    public function store(Request $request) {
+        $recette = new Recette();
+        $recette->nom = $request->input('nom');
+        $recette->description = $request->input('description');
+        $recette->category = $request->input('category');
+        $recette->visuel = $request->input('visuel');
+        $recette->temps_preparation = $request->input('temps_preparation');
+        $recette->nb_personnes = $request->input('nb_personnes');
+        $recette->cout = $request->input('cout');
+        $recette->save();
+
+        return redirect()->route('recettes.index');
+    }
+
+    public function edit($id) {
+        $recette = Recette::find($id);
+        return view('recettes.edit', ['recette' => $recette, 'titre' => "Éditer une recette"]);
+    }
+
+    public function update($id, Request $request) {
+        $recette = Recette::find($id);
+        $recette->nom = $request->input('nom');
+        $recette->description = $request->input('description');
+        $recette->category = $request->input('category');
+        $recette->visuel = $request->input('visuel');
+        $recette->temps_preparation = $request->input('temps_preparation');
+        $recette->nb_personnes = $request->input('nb_personnes');
+        $recette->cout = $request->input('cout');
+        $recette->save();
+
+        return redirect()->route('recettes.index');
+    }
+
+    public function destroy($id) {
+        $recette = Recette::find($id);
+        $recette->delete();
+
+        return redirect()->route('recettes.index');
+    }
+
+    public function search(Request $request) {
+        $search = $request->input('search');
+        $recettes = Recette::where('nom', 'LIKE', "%$search%")
+                              ->orWhere('description', 'LIKE', "%$search%")
+                              ->orWhere('category', 'LIKE', "%$search%")
+                              ->get();
+
+        return view('recettes.index', ['titre' => "Recherche", 'recettes' => $recettes]);
+    }
 }
