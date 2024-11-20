b1a9d7ba18440aa8bc46fb6519e288a929810f36
samuel_tittelein <samuel_tittelein@ens.univ-artois.fr>
2024-11-20 10:53:56 +0100

    better interface

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,6 +1,9 @@
 <!-- resources/views/components/menu.blade.php -->
 <nav>
-{{--    <button><a href="{{route('accueil')}}">🏛Accueil</a></button>
-    <button><a href="{{route('taches.index')}}">📜 Taches</a></button>--}}
+    <button><a href="{{route('accueil')}}">🏛Accueil</a></button>
+    <button><a href="/presentation">📜 presentation</a></button>
+    <button><a href="/contact">Contact</a></button>
+    <button><a href="/recettes">Recettes</a></button>
+
     <button>😎 Connexion</button>
 </nav>
